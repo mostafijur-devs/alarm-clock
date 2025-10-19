daa47daa0bd79634edf6164a855edf79a9ae4bf7
Mostafijur Rahman <mostafijur.devs@gmail.com>
2025-10-19 18:01:02 +0600

    alarm set

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,122 @@
+// import 'dart:io';
+//
+// import 'package:alarm/alarm.dart';
+// import 'package:flutter/material.dart';
+// import 'package:permission_handler/permission_handler.dart';
+//
+// void main() async{
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Alarm.init();
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//
+//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+//       ),
+//       home: const MyHomePage(title: 'Flutter Demo Home Page'),
+//     );
+//   }
+// }
+//
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({super.key, required this.title});
+//
+//   final String title;
+//
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+//
+// class _MyHomePageState extends State<MyHomePage> {
+//
+//   @override
+//   void initState() {
+//     // TODO: implement initState
+//     super.initState();
+//   }
+//   Future<void> checkAndroidScheduleExactAlarmPermission() async {
+//     final status = await Permission.scheduleExactAlarm.status;
+//     print('Schedule exact alarm permission: $status.');
+//     if (status.isDenied) {
+//       print('Requesting schedule exact alarm permission...');
+//       final res = await Permission.scheduleExactAlarm.request();
+//       print('Schedule exact alarm permission ${res.isGranted ? '' : 'not'} granted.');
+//     }
+//   }
+//
+//   int _counter = 0;
+//
+//   void _incrementCounter() {
+//     setState(() {
+//       _counter++;
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//
+//     return Scaffold(
+//       appBar: AppBar(
+//
+//         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+//
+//         title: Text(widget.title),
+//       ),
+//       body: Center(
+//         // Center is a layout widget. It takes a single child and positions it
+//         // in the middle of the parent.
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             const Text('You have pushed the button this many times:'),
+//             Text(
+//               '$_counter',
+//               style: Theme.of(context).textTheme.headlineMedium,
+//             ),
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed:() {
+//
+//           final alarmDate = DateTime.now().add(Duration(seconds: 30));
+//           final alarmSettings = AlarmSettings(
+//             id: 42,
+//             dateTime: alarmDate,
+//             assetAudioPath: 'assets/alarm.mp3',
+//             loopAudio: true,
+//             vibrate: true,
+//             volumeSettings: VolumeSettings.fade(fadeDuration: Duration(seconds: 3)),
+//             notificationSettings: NotificationSettings(title: 'this is tittle', body: 'this is body',),
+//           );
+//
+//         } ,
+//         tooltip: 'Increment',
+//         child: const Icon(Icons.add),
+//       ), // This trailing comma makes auto-formatting nicer for build methods.
+//     );
+//   }
+// }
+import 'dart:async';
+import 'package:alarm/alarm.dart';
+import 'package:alarm_clock/home_screen.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Alarm.init();
   runApp(const MyApp());
 }
 
@@ -13,39 +129,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Alarm Demo'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -54,69 +147,171 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  late List<AlarmSettings> alarms = [];
 
-  void _incrementCounter() {
+  static StreamSubscription<AlarmSettings>? subscription;
+
+  int pickHours = 0;
+  int pickMinute = 0;
+
+
+  @override
+  void initState() {
+    super.initState();
+    //notifcation permission
+    checkAndroidNotificationPermission();
+    //schedule alarm permission
+    checkAndroidScheduleExactAlarmPermission();
+    loadAlarms();
+    subscription ??= Alarm.ringStream.stream.listen(navigateToRingScreen);
+    //listen alarm if active than navigate to alarm screen
+  }
+
+  Future<void> loadAlarms() async {
+    final loadedAlarms = await Alarm.getAlarms();
+    loadedAlarms.sort((a, b) => a.dateTime.compareTo(b.dateTime));
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      alarms = loadedAlarms;
     });
+  }
+
+  Future<void> checkAndroidNotificationPermission() async {
+    final status = await Permission.notification.status;
+    if (status.isDenied) {
+      print('Requesting notification permission...');
+      final res = await Permission.notification.request();
+      print('Notification permission ${res.isGranted ? '' : 'not '}granted');
+    }
+  }
+
+  Future<void> navigateToRingScreen(AlarmSettings alarmSettings) async {
+    await Navigator.push(
+      context,
+      MaterialPageRoute<void>(
+        builder: (context) =>
+            AlarmNotificationScreen(alarmSettings: alarmSettings),
+      ),
+    );
+    loadAlarms();
+  }
+
+  Future<void> checkAndroidScheduleExactAlarmPermission() async {
+    final status = await Permission.scheduleExactAlarm.status;
+    if (kDebugMode) {
+      print('Schedule exact alarm permission: $status.');
+    }
+    if (status.isDenied) {
+      if (kDebugMode) {
+        print('Requesting schedule exact alarm permission...');
+      }
+      final res = await Permission.scheduleExactAlarm.request();
+      if (kDebugMode) {
+        print(
+          'Schedule exact alarm permission ${res.isGranted
+              ? ''
+              : 'not'} granted.',
+        );
+      }
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+        backgroundColor: Theme
+            .of(context)
+            .colorScheme
+            .inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: ListView(
+          children: [
+            Column(
+              children: List.generate(
+                alarms.length,
+                    (index) => ListTile(
+                  title: Text(DateFormat('hh:mm a').format(alarms[index].dateTime)),
+                ),
+              ),
+            )
+          ]
+
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: () async {
+          Navigator.push(context, MaterialPageRoute(builder: (context) =>
+              HomeScreen(
+                onTimeSelected: (hour, minute) {
+                  setState(() {
+                    pickHours = hour;
+                    pickMinute = minute;
+                  });
+                  print( "Home Screen time $hour, $minute");
+                },
+              )));
+          loadAlarms();
+        },
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+    );
+  }
+}
+
+class AlarmNotificationScreen extends StatefulWidget {
+  final AlarmSettings alarmSettings;
+
+  const AlarmNotificationScreen({super.key, required this.alarmSettings});
+
+  @override
+  State<AlarmNotificationScreen> createState() =>
+      _AlarmNotificationScreenState();
+}
+
+class _AlarmNotificationScreenState extends State<AlarmNotificationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text("Alram is ringing......."),
+          Text(widget.alarmSettings.notificationSettings.title),
+          Text(widget.alarmSettings.notificationSettings.body),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  //skip alarm for next time
+                  final now = DateTime.now();
+                  Alarm.set(
+                    alarmSettings: widget.alarmSettings.copyWith(
+                      dateTime: DateTime(
+                        now.year,
+                        now.month,
+                        now.day,
+                        now.hour,
+                        now.minute,
+                      ).add(const Duration(minutes: 1)),
+                    ),
+                  ).then((_) => Navigator.pop(context));
+                },
+                child: const Text("Snooze"),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  //stop alarm
+                  Alarm.stop(
+                    widget.alarmSettings.id,
+                  ).then((_) => Navigator.pop(context));
+                },
+                child: const Text("Stop"),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
